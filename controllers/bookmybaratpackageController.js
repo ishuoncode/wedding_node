1ce77aa001a6be9b3503247958c08269eb66c3a9
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2025-02-19 20:36:18 +0530

    small changes

--- controllers/bookmybaratpackageController.js
+++ controllers/bookmybaratpackageController.js
@@ -2,32 +2,45 @@ const BookMyBaratPackage = require("../models/bookmybaratPackageModel");
 
 // Create a new package
 exports.createPackage = async (req, res) => {
-    try {
-      let packages;
-      if (Array.isArray(req.body)) {
-        // If the input is an array, create multiple packages
-        packages = await BookMyBaratPackage.insertMany(req.body, { ordered: false });
-      } else {
-        // If it's a single object, create one package
-        const newPackage = new BookMyBaratPackage(req.body);
-        packages = [await newPackage.save()];
-      }
-      res.status(201).json(packages);
-    } catch (error) {
-      if (error.code === 11000) {
-        // Handle duplicate key error
-        res.status(400).json({ message: "Duplicate package name(s) found. Some packages may not have been created." });
-      } else {
-        res.status(400).json({ message: error.message });
-      }
+  try {
+    let packages;
+    if (Array.isArray(req.body)) {
+      // If the input is an array, create multiple packages
+      packages = await BookMyBaratPackage.insertMany(req.body, {
+        ordered: false,
+      });
+    } else {
+      // If it's a single object, create one package
+      const newPackage = new BookMyBaratPackage(req.body);
+      packages = [await newPackage.save()];
     }
-  };
+    res.status(201).json({
+      message: "success",
+      length: packages.length,
+      data: packages,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      // Handle duplicate key error
+      res.status(400).json({
+        message:
+          "Duplicate package name(s) found. Some packages may not have been created.",
+      });
+    } else {
+      res.status(400).json({ message: error.message });
+    }
+  }
+};
 
 // Get all packages
 exports.getAllPackages = async (req, res) => {
   try {
     const packages = await BookMyBaratPackage.find();
-    res.status(200).json(packages);
+    res.status(200).json({
+      message: "success",
+      length: packages.length,
+      data: packages,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -40,7 +53,7 @@ exports.getPackageById = async (req, res) => {
     if (!package) {
       return res.status(404).json({ message: "Package not found" });
     }
-    res.status(200).json(package);
+    res.status(200).json({ message: "success", data: package });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -66,7 +79,9 @@ exports.updatePackage = async (req, res) => {
 // Delete a package by ID
 exports.deletePackage = async (req, res) => {
   try {
-    const deletedPackage = await BookMyBaratPackage.findByIdAndDelete(req.params.id);
+    const deletedPackage = await BookMyBaratPackage.findByIdAndDelete(
+      req.params.id
+    );
     if (!deletedPackage) {
       return res.status(404).json({ message: "Package not found" });
     }
